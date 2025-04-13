84f0096a64e7e3f5d9b8d703bb6195051db55ddb
Ahmed Ibrahim <ahmedibrahim000074@gmail.com>
2025-04-13 14:44:18 +0200

    added lab 3.2

---
+++ lab3/lab3.2/index.js
@@ -0,0 +1,29 @@
+// let nameArr = new Array();
+// for (let i = 0; i < 10; i++) 
+// {
+//     // Prompt the user to enter a name and convert it to lowercase
+//     nameArr[i] = prompt("Enter name " + (i + 1) + ": ").toLowerCase();
+// }
+let nameArr = ["Ahmed","mahmoud","ezat", "Ali", "Ali", "Ali", "Ahmed", "Ahmed", "Ali", "Ali", "Ahmed", "Ahmed"];
+let uniqueNames = [];
+for (let i = 0; i < nameArr.length; i++) {
+    if (!uniqueNames.includes(nameArr[i])) {
+        uniqueNames.push(nameArr[i]);
+    }
+}
+console.log(uniqueNames);
+const randonTwoNames = (uniqueNames) => {
+    let randomNames = [];
+    let randoms = [];
+    do {
+        let random = Math.floor(Math.random() * uniqueNames.length);
+        if (!randoms.includes(random)) {
+            randoms.push(random);
+            randomNames.push(uniqueNames[random]);
+        }
+
+    } while (randoms.length < 2);
+    return randomNames;
+}
+
+console.log(randonTwoNames(uniqueNames));
